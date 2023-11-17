3a610b38575382e04b3c0bef07302e3feea85872
Manuel <manuelriedel002@gmail.com>
2023-11-17 19:34:26 -0300

    comienzo con el punto 1, revisar InoticiasService

---
+++ proyecto/aspnet-core/src/proyecto.Application.Contracts/Noticias/INoticiasAppService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto.Noticias
+{
+    public interface INoticiasAppService
+    {
+        Task<ICollection<NoticiaDto>> Search(string query);
+    }
+}
---
+++ proyecto/aspnet-core/src/proyecto.Domain.Shared/ArticuloDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace proyecto
+{
+    /// <summary>
+    /// Este dto representa la respuesta de la api de noticias. Lo pongo en este proyecto ya que son diferentes a los dtos de application
+    /// </summary>
+    public class ArticuloDto
+    {
+        public string Autor { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public string Url { get; set; }
+
+        public string UrlDeImagen { get; set; }
+
+        public DateTime? FechaPublicado { get; set; }
+
+        public string Contenido { get; set; }
+    }
+}
