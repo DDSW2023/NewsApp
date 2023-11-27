4352f6f60a29dc2365fc25d935efe1bb3f7bc835
Manuel <manuelriedel002@gmail.com>
2023-11-27 04:37:57 -0300

    tests punto 2

---
+++ proyecto/aspnet-core/test/proyecto.Application.Tests/ListaNoticias/ListaNoticiasAppService_Test.cs
@@ -0,0 +1,107 @@
+﻿using proyecto.EntityFrameworkCore;
+using proyecto.ListaNoticiasDto;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.Uow;
+using Xunit;
+
+namespace proyecto.ListaNoticias
+{
+    public class ListaNoticiasAppService_Test : proyectoApplicationTestBase
+    {
+        private readonly IListaNoticiasAppService _listaNoticiasAppService;
+        private readonly IDbContextProvider<proyectoDbContext> _dbContextProvider;
+        private readonly IUnitOfWorkManager _unitOfWorkManager;
+
+        public ListaNoticiasAppService_Test()
+        {
+            _listaNoticiasAppService = GetRequiredService<IListaNoticiasAppService>();
+            _dbContextProvider = GetRequiredService<IDbContextProvider<proyectoDbContext>>();
+            _unitOfWorkManager = GetRequiredService<IUnitOfWorkManager>();
+        }
+
+        [Fact]
+        public async Task Should_Get_All_Themes()
+        {
+            //Act
+            var listaNoticias = await _listaNoticiasAppService.GetThemesAsync();
+            //Assert
+            listaNoticias.ShouldNotBeNull();
+            listaNoticias.Count.ShouldBeGreaterThan(1);
+        }
+
+        [Fact]
+        public async Task Should_Create_ListaNoticias()
+        {
+            //Arrange            
+            var input = new CrearListaNoticiaDto { nombreLista = "nueva lista noticias" };
+
+            //Act
+            var newListaNoticias = await _listaNoticiasAppService.CreateListaNoticiaAsync(input);
+
+            //Assert
+            // Se verifican los datos devueltos por el servicio
+            newListaNoticias.ShouldNotBeNull();
+            newListaNoticias.Id.ShouldBePositive();
+            // se verifican los datos persistidos por el servicio
+            using (var uow = _unitOfWorkManager.Begin())
+            {
+                var dbContext = await _dbContextProvider.GetDbContextAsync();
+                dbContext.ListaNoticias.FirstOrDefault(t => t.Id == newListaNoticias.Id).ShouldNotBeNull();
+                dbContext.ListaNoticias.FirstOrDefault(t => t.Id == newListaNoticias.Id).nombreLista.ShouldBe(input.nombreLista);
+            }
+        }
+
+        [Fact]
+        public async Task Should_Update_ListaNoticias()
+        {
+            //Arrange            
+            var input = new CrearListaNoticiaDto { nombreLista = "nueva lista noticias", Id = 1 };
+
+            //Act
+            var newListaNoticias = await _listaNoticiasAppService.CreateListaNoticiaAsync(input);
+
+            //Assert
+            // Se verifican los datos devueltos por el servicio
+            newListaNoticias.ShouldNotBeNull();
+            newListaNoticias.Id.ShouldBePositive();
+            // se verifican los datos persistidos por el servicio
+            using (var uow = _unitOfWorkManager.Begin())
+            {
+                var dbContext = await _dbContextProvider.GetDbContextAsync();
+                dbContext.ListaNoticias.FirstOrDefault(t => t.Id == newListaNoticias.Id).ShouldNotBeNull();
+                dbContext.ListaNoticias.FirstOrDefault(t => t.Id == newListaNoticias.Id).nombreLista.ShouldBe(input.nombreLista);
+            }
+        }
+
+        [Fact]
+        public async Task Should_Create_Child_ListaNoticias()
+        {
+            //Arrange            
+            var input = new CrearListaNoticiaDto { nombreLista = "nuevo ListaNoticias hija", ParentId = 1 };
+
+            //Act
+            var newListaNoticias = await _listaNoticiasAppService.CreateListaNoticiaAsync(input);
+
+            //Assert
+            // Se verifican los datos devueltos por el servicio
+            newListaNoticias.ShouldNotBeNull();
+            newListaNoticias.Id.ShouldBePositive();
+            // se verifican los datos persistidos por el servicio
+            using (var uow = _unitOfWorkManager.Begin())
+            {
+                var dbContext = await _dbContextProvider.GetDbContextAsync();
+                dbContext.ListaNoticias.FirstOrDefault(t => t.Id == newListaNoticias.Id).ShouldNotBeNull();
+                dbContext.ListaNoticias.FirstOrDefault(t => t.Id == newListaNoticias.Id).nombreLista.ShouldBe(input.nombreLista);
+            }
+        }
+
+
+
+    }
+}
---
+++ proyecto/aspnet-core/test/proyecto.Domain.Tests/ListaNoticias/ListaNoticiasManager_Integration_Tests.cs
@@ -0,0 +1,52 @@
+﻿using proyecto.ListaNoticias;
+using Volo.Abp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.AspNetCore.Identity;
+using Volo.Abp.Identity;
+using Shouldly;
+using Volo.Abp.Users;
+using Volo.Abp.Security.Claims;
+using Volo.Abp.Domain.Repositories;
+
+namespace proyecto.ListaNoticia
+{
+    public class ListaNoticiaManager_Integration_Tests : proyectoDomainTestBase
+    {
+        private readonly ListaNoticiaManager _listaNoticiaManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ICurrentUser _currentUser;
+        private readonly ICurrentPrincipalAccessor _currentPrincipalAccessor;
+        private readonly IRepository<IdentityUser, Guid> _identityRepository;
+
+        public ListaNoticiaManager_Integration_Tests()
+        {
+            _listaNoticiaManager = GetRequiredService<ListaNoticiaManager>();
+            _userManager = GetRequiredService<UserManager<IdentityUser>>();
+            _currentUser = GetRequiredService<ICurrentUser>();
+            _currentPrincipalAccessor = GetRequiredService<ICurrentPrincipalAccessor>();
+            _identityRepository = GetRequiredService<IRepository<IdentityUser, Guid>>();
+        }
+
+        [Fact]
+        public async Task Should_Assign_An_Issue_To_A_User()
+        {
+            // Arrange
+            int? id = null;
+            var name = "ListaNoticia agregado";
+            int? parentId = null;
+            var user = await _userManager.FindByIdAsync(_currentUser.Id.GetValueOrDefault().ToString());
+            var user2 = await _identityRepository.GetAsync(_currentUser.Id.GetValueOrDefault());
+
+            // Act
+            var listaNoticia = await _listaNoticiaManager.CreateAsyncOrUpdate(id, name, parentId, user2);
+
+            //Assert
+            listaNoticia.ShouldNotBeNull();
+        }
+    }
+}
