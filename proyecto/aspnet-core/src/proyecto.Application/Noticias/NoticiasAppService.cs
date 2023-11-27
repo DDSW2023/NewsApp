1ca8cddc96cc4ef3182155c6a91b4761abc1aeda
Manuel <manuelriedel002@gmail.com>
2023-11-27 00:39:12 -0300

    punto 2

--- proyecto/aspnet-core/src/proyecto.Application/Noticias/NoticiasAppService.cs
+++ proyecto/aspnet-core/src/proyecto.Application/Noticias/NoticiasAppService.cs
@@ -2,24 +2,6 @@
 using System.Threading.Tasks;
 using proyecto.noticias;
 
-<<<<<<< HEAD
-
-namespace proyecto.Noticias
-{
-    public class NoticiasAppService : proyectoAppService, INoticiasAppService
-    {
-        private readonly INoticiasService _noticiasService;
-
-        public NoticiasAppService(INoticiasService noticiasService)
-        {
-            _noticiasService = noticiasService;
-        }
-
-        public Task<ICollection<NoticiaDto>> Search(string query)
-        {
-            throw new NotImplementedException();
-        }
-=======
 namespace proyecto.Noticias;
 
 public class NoticiasAppService : proyectoAppService, INoticiasAppService
@@ -30,7 +12,6 @@ public class NoticiasAppService : proyectoAppService, INoticiasAppService
     public NoticiasAppService(INoticiasService newsService)
     {
         _noticiasService = newsService;
->>>>>>> 0a4434e97851e0decfdb5a6dcd0d967255475149
     }
     
     public async Task<ICollection<ArticuloDto>> Search(string query)
--- proyecto/aspnet-core/src/proyecto.Domain/noticias/INoticiasService.cs
+++ proyecto/aspnet-core/src/proyecto.Domain/noticias/INoticiasService.cs
@@ -1,19 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
-<<<<<<< HEAD
-namespace NewsApp.News
-{
-    public interface INewsService
-    {
-        Task<ICollection<ArticuloDto>> GetNewsAsync(string query);
-    }
-=======
+
 namespace proyecto.noticias;
 
 public interface INoticiasService
 {
     Task<ICollection<ArticuloDto>> GetNewsAsync(string query);
 
->>>>>>> 0a4434e97851e0decfdb5a6dcd0d967255475149
+
 }
